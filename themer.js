f4b9ec8a112e2f1d50ffa33877158e12c608004f
Louis-ctrl <louismjoseph0@gmail.com>
2026-04-21 09:34:39 -0400

    Dark mode functionality added

---
+++ themer.js
@@ -0,0 +1,23 @@
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+localStorage.setItem("theme", currentThemeSetting);
+updateThemeOnHtmlEl({ theme: currentThemeSetting });    
